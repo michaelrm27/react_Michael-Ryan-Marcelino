5373389a28d889fd8ca0bbe5328b9aae08fd3eec
michaelrm27 <michaelmarcelino55@gmail.com>
2023-09-13 20:29:14 +0700

    feat: add html css and javascript

---
+++ codeCompetence1/assets/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+
+function alertFucntion() {
+  return alert("Form has been Submitted");
+}
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault;
+//   alertFucntion();  
+//   form.reset();
+// });
