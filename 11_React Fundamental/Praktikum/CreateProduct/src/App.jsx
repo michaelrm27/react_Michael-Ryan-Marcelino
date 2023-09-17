5177de82e31e4078163a2b356a8af1112b846ef9
michaelrm27 <michaelmarcelino55@gmail.com>
2023-09-17 12:08:36 +0700

    feat: Add CSS, JS, and Create Component

---
+++ 11_React Fundamental/Praktikum/CreateProduct/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Form from './Components/Form'
+import Header from './Components/Header'
+import Navbar from './Components/Navbar'
+import Table from './Components/Table'
+import "./assets/JS/CreateProductscript.js"
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <Header/>
+     <Form/>
+     <Table/>
+    </>
+  )
+}
+
+export default App
