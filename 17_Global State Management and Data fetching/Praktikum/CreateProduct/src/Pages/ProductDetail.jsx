dc093079e7b9e2726e9ded7071582c596b996aac
michaelrm27 <michaelmarcelino55@gmail.com>
2023-09-26 16:03:38 +0700

    feat: Add more table header

---
+++ 17_Global State Management and Data fetching/Praktikum/CreateProduct/src/Pages/ProductDetail.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useProductContext } from "./ProductContext";
+
+export default function Detail() {
+  const { id } = useParams();
+  const { tableData } = useProductContext();
+  const product = tableData[id - 1];
+
+  if (!product) {
+    return <div className="justify-content-center text-center">Product Not Found</div>;
+  }
+
+  return (
+    <div className="flex justify-content-center text-center">
+      <h1>Product Detail</h1>
+      <p>Product Name: {product.productName} </p>
+      <p>Product Category: {product.productCategory} </p>
+      <p>Product Image: {product.productImage} </p>
+      <p>Product Freshness: {product.productFreshness} </p>
+      <p>Product Description: {product.productDesc} </p>
+      <p>Product Price: {product.productPrice} </p>
+    </div>
+  );
+}
